365a9dbf10af334375ffe5d6a20a36fafc86bc95
kurtreyn <kurt.reynolds@twinspires.com>
2026-05-27 12:42:19 -0400

    send & receive

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
 #!/usr/bin/env python
 import asyncio
 from websockets.asyncio.server import serve
+import json
+from connect4 import PLAYER1, PLAYER2
 # from websockets.exceptions import ConnectionClosedOK
 
 
 async def handler(websocket):
-    async for message in websocket:
-        print(f"Received: {message}")
+    for player, column, row in [
+        (PLAYER1, 3, 0),
+        (PLAYER2, 3, 1),
+        (PLAYER1, 4, 0),
+        (PLAYER2, 4, 1),
+        (PLAYER1, 2, 0),
+        (PLAYER2, 1, 0),
+        (PLAYER1, 5, 0),
+    ]:
+        event = {
+            "type": "play",
+            "player": player,
+            "column": column,
+            "row": row,
+        }
+        await websocket.send(json.dumps(event))
+        await asyncio.sleep(0.5)
+    event = {
+        "type": "win",
+        "player": PLAYER1,
+    }
+    await websocket.send(json.dumps(event))
 
 
 async def main():
